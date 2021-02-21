d03042a4a2c639ec92e5b54da1673e6549585e9b
Ubsefor <letzdropthebas@gmail.com>
2021-02-21 13:01:44 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn usage() {
+    println!("Usage: rs-genome <genome file path>\nNote, that file must not contain header!");
+}
+
+fn version(){
+    println!("version: 0.0.1");
+}
+
+fn count_genes(genome : String, gene : char) -> usize {
+    return genome.matches(gene).count();
+}
+
+fn count_cg(genome : String) -> usize {
+    return genome.matches("CG").count();
+}
+
+fn count_ta(genome : String) -> usize {
+    return genome.matches("TA").count()
+}
+
+fn check_skew(genome : String) -> bool{
+    let length = genome.len();
+    let half = length / 2;
+    let first_half = &genome[0..half];
+    let second_half = &genome[(half + 1)..];
+    let count_c = first_half.matches("C").count();
+    let count_g = first_half.matches("G").count();
+    let count_c_last = second_half.matches("C").count();
+    let count_g_last = second_half.matches("G").count();
+    return count_c > count_g && count_c_last < count_g_last;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            usage();
+            return;
+        },
+        
+        2 => {
+            if  &args[1] == "-h" {
+                usage();
+                return;
+            }
+            if &args[1] == "-v"{
+                version();
+                return;
+            }
+            let filepath = &args[1];
+            if !(Path::new(filepath).is_file()){
+                panic!("Error: couldn't read file at specified filepath!");
+            }
+            let contents = fs::read_to_string(filepath)
+                .expect("Something went wrong reading the file");
+            if !(contents.chars().nth(0).unwrap() == 'A' 
+                    || contents.chars().nth(0).unwrap() == 'C'
+                    || contents.chars().nth(0).unwrap() == 'G' 
+                    || contents.chars().nth(0).unwrap() == 'T') {
+                panic!("Error: file seems to contain a header or not a raw genome!")
+            }
+            println!("Parsing genome in file: {}", filepath);
+            println!("Total genome size: {}", contents.len());
+            println!("Amount of A: {}", count_genes(contents.clone(), 'A'));
+            println!("Amount of C: {}", count_genes(contents.clone(), 'C'));
+            println!("Amount of T: {}", count_genes(contents.clone(), 'T'));
+            println!("Amount of G: {}", count_genes(contents.clone(), 'G'));
+            println!("Amount of CG: {}", count_cg(contents.clone()));
+            println!("Amount of TA: {}", count_ta(contents.clone()));
+            println!("GC Skew is present: {}", check_skew(contents));
+            return ;
+        },
+        
+        _ => {
+            usage();
+            return;
+        }
+    }
+}
+
+
+
+
